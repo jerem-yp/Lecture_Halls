194779eec21f1e0e367a29282fb541930f683fb5
Jeremy Peros <jeremy.peros@gmail.com>
2023-11-10 18:34:21 -0800

    Basic queries can now be made to access the database by day or by classroom

--- backend/local_backend_sql/db_connection.py
+++ backend/local_backend_sql/db_connection.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-
+from typing import Generator
 DATABASE_PATH = "schedule.db"
 
 """ Create databases."""
@@ -80,3 +80,159 @@ class Database_Querying:
                     FOREIGN KEY (courseID) REEFERENCES Courses(courseID)
                     );""")
 
+    # Add rows
+    def insert_row(self, *, courseID: int, courseTitle: str, location: str, days: str, time_start: str, time_end: str) -> None:
+        """ This function takes data from the classes and adds it to the database."""
+        # First, check if the course is already inside. If it is, no addition to be made.
+        if not self.row_already_exists(courseID=courseID):
+            cur = self.connection.cursor()
+            data_tuple = (courseID, courseTitle, location)
+            cur.execute("""
+                        INSERT INTO Courses 
+                        VALUES (?, ?, ?);
+                        """, data_tuple)
+            self._insert_into_tables(courseID=courseID, days=days, time_start=time_start, time_end=time_end)
+
+
+
+    def _insert_into_tables(self, *, courseID: int, days: str, time_start: str, time_end: str) -> None:
+        """ This function takes time data and inserts into any tables."""
+        data_tuple = (courseID, time_start, time_end)
+        cur = self.connection.cursor()
+        if 'M' in days:  # Insert into Monday table
+            cur.execute("""
+                        INSERT INTO Monday
+                        VALUES (?, ?, ?);""", data_tuple)
+
+        if 'Tu' in days: # Insert into Tuesday table
+            cur.execute("""
+                        INSERT INTO Tuesday
+                        VALUES (?, ?, ?);""", data_tuple)
+
+        if 'W' in days:  # Insert into Wednesday table
+            cur.execute("""
+                        INSERT INTO Wednesday
+                        VALUES (?, ?, ?);""", data_tuple)
+
+        if 'Th' in days:  # Insert into Thursday table
+            cur.execute("""
+                        INSERT INTO Thursday
+                        VALUES (?, ?, ?);""", data_tuple)
+
+        if 'F' in days:  # Insert into Friday table
+            cur.execute("""
+                        INSERT INTO Monday
+                        VALUES (?, ?, ?);""", data_tuple)
+
+    # Check if course is already in DB.
+    def row_already_exists(self, *, courseID: int) -> bool:
+        """ Check if the courseID already exists. """
+        cur = self.connection.cursor()
+        placeholder = (courseID, )
+        cur.execute(f"""
+                    SELECT courseID
+                    FROM Courses
+                    WHERE courseID = ?
+                    );""", placeholder)
+        res = cur.fetchall()
+        return len(res) > 0
+
+    # Queries to get data from this DB
+    def get_all_for_day(self, day: str) -> Generator[tuple]:
+        """ Given a day (Monday, Tuesday, Wednesday, Thursday, Friday), find all classes and times on that day."""
+        cur = self.connection.cursor()
+        if day == 'Monday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Monday on Monday.courseID = Courses.courseID
+                        );""")
+
+        elif day == 'Tuesday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Tuesday on Tuesday.courseID = Courses.courseID
+                        );""")
+
+        elif day == 'Wednesday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Wednesday on Wednesday.courseID = Courses.courseID
+                        );""")
+
+        elif day == 'Thursday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Thursday on Thursday.courseID = Courses.courseID
+                        );""")
+
+        elif day == 'Friday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Friday on Friday.courseID = Courses.courseID
+                        );""")
+
+        else:
+            raise Exception("Day is invalid.")
+
+        res = cur.fetchone()
+        while res:
+            yield res
+            res = cur.fetchone()
+
+    def get_all_for_class(self, day: str, cls_name: str) -> Generator[tuple]:
+        """ For a single classroom, get all results."""
+        """ Given a day (Monday, Tuesday, Wednesday, Thursday, Friday), find all classes and times on that day."""
+        cur = self.connection.cursor()
+        loc = (cls_name, )
+        if day == 'Monday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Monday on Monday.courseID = Courses.courseID
+                        WHERE Courses.location = ?
+                        );""", loc)
+
+        elif day == 'Tuesday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Tuesday ON Tuesday.courseID = Courses.courseID
+                        WHERE Courses.location = ?
+                        );""", loc)
+
+        elif day == 'Wednesday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Wednesday on Wednesday.courseID = Courses.courseID
+                        WHERE Courses.location = ?
+                        );""", loc)
+
+        elif day == 'Thursday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Thursday on Thursday.courseID = Courses.courseID
+                        WHERE Courses.location = ?
+                        );""", loc)
+
+        elif day == 'Friday':
+            cur.execute("""
+                        SELECT courseID, location, time_start, time_end
+                        FROM Courses
+                        INNER JOIN Friday on Friday.courseID = Courses.courseID
+                        WHERE Courses.location = ?
+                        );""", loc)
+
+        else:
+            raise Exception("Day is invalid.")
+
+        res = cur.fetchone()
+        while res:
+            yield res
+            res = cur.fetchone()
